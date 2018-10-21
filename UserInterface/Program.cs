cc37394b0a9e40b1a8a28854860472e2647a210b
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-21 20:43:23 +0300

    Create Project Called MediaLibrary

---
+++ UserInterface/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
