2c03d08632d70257419408fd7c94fda637e8e9ce
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-23 09:48:47 +0300

    Write Fields and Properties for File

---
+++ FileEnv/File.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Interfaces.FileEnv;
+
+namespace FileEnv
+{
+    abstract class File : IFile
+    {
+        private Guid _id;
+        private string _name;
+        private string _type;
+        private ulong _size; // byte
+        private string _location;
+
+        public Guid Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Type { get => _type; set => _type = value; }
+        public ulong Size { get => _size; set => _size = value; }
+        public string Location { get => _location; set => _location = value; }
+
+        public File(string type, string name, ulong size, string location)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Type = type;
+            Size = size;
+            Location = location;
+        }
+
+        abstract public void Accept(IVisitor visitor);
+    }
+}
