c7ae3caebc730964ba69a74ee65f247ffbf6212a
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-23 10:07:13 +0300

    Write Fields and Properties for Photo

---
+++ FileEnv/Photo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Interfaces.FileEnv;
+
+namespace FileEnv
+{
+    class Photo : File, IPhoto
+    {
+        private string _dimensions;
+        private ushort _width;
+        private ushort _height;
+
+        public Photo(string type, string name, ulong size, string location, // File
+            string dimensions, ushort width, ushort height) : // Photo
+            base(type, name, size, location)
+        {
+            Dimensions = dimensions;
+            Width = width;
+            Height = height;
+        }
+
+        public string Dimensions { get => _dimensions; set => _dimensions = value; }
+        public ushort Width { get => _width; set => _width = value; }
+        public ushort Height { get => _height; set => _height = value; }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.VisitPhotoFile(this);
+        }
+    }
+}
