6c6e931b9499169eee2b60785275c949c44d8b17
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-23 09:52:10 +0300

    Write Fields and Properties for PlayTime

---
+++ FileEnv/PlayTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Interfaces.FileEnv;
+
+namespace FileEnv
+{
+    abstract class PlayTime : File, IPlayTime
+    {
+        private string _length;
+
+        public PlayTime(string type, string name, ulong size, string location, string length) :
+            base(type, name, size, location)
+        {
+            Length = length;
+        }
+
+        public string Length { get => _length; set => _length = value; }
+    }
+}
