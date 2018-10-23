8f04d0c9fde0d43f0d83154b18e4105fdeb971b5
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-23 10:05:00 +0300

    Write Fields and Properties for Music

---
+++ FileEnv/Music.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Interfaces.FileEnv;
+
+namespace FileEnv
+{
+    class Music : AudioParameters, IMusic
+    {
+        private string _genre;
+        private string _artist;
+        private string _album;
+        private string _title;
+        private string _year;
+
+        public Music(string type, string name, ulong size, string location, // File
+            ulong bitRate, ulong sampleRate, // AudioParameters
+            string length, // PlayTime
+            string genre, string artist, string album, string title, string year) : // Music
+            base(type, name, size, location, length, bitRate, sampleRate)
+        {
+            Genre = genre;
+            Artist = artist;
+            Album = album;
+            Title = title;
+            Year = year;
+        }
+
+        public string Genre { get => _genre; set => _genre = value; }
+        public string Artist { get => _artist; set => _artist = value; }
+        public string Album { get => _album; set => _album = value; }
+        public string Title { get => _title; set => _title = value; }
+        public string Year { get => _year; set => _year = value; }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.VisitMusicFile(this);
+        }
+    }
+}
