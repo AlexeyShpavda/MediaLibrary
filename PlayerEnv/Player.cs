3a27d27bdde52d3d635141b57fcce4003b64f56e
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-24 10:42:26 +0300

    Implement IVisitor Interface in Player

---
+++ PlayerEnv/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces.FileEnv;
+using Interfaces.PlayerEnv;
+
+namespace PlayerEnv
+{
+    public class Player : IPlayer, IVisitor
+    {
+        public void VisitMusicFile(IMusic music)
+        {
+        }
+
+        public void VisitPhotoFile(IPhoto photo)
+        {
+        }
+
+        public void VisitVideoFile(IVideo video)
+        {
+        }
+    }
+}
