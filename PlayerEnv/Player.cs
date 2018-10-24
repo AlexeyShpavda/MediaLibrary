87499be7cca7596c087aae236db53cd4712e678f
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-24 10:46:26 +0300

    Implement IPlayer Interface in Player

--- PlayerEnv/Player.cs
+++ PlayerEnv/Player.cs
@@ -5,11 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using Interfaces.FileEnv;
 using Interfaces.PlayerEnv;
+using Interfaces.PlaylistEnv;
 
 namespace PlayerEnv
 {
     public class Player : IPlayer, IVisitor
     {
+        public void Play(ICollection<IFile> files)
+        {
+            foreach(var file in files)
+            {
+                file.Accept(this);
+            }
+        }
+
+        public void PlayFile(IFile file)
+        {
+            file.Accept(this);
+        }
+
+        public void PlayPlaylist(IPlaylist<IFile> playlist)
+        {
+            foreach (var file in playlist.GetAllFiles())
+            {
+                file.Accept(this);
+            }
+        }
+
         public void VisitMusicFile(IMusic music)
         {
         }
