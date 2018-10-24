02f5d61924efaa6bfb511ed29546fe169e0abdc6
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-24 10:51:19 +0300

    Remove Implementation of IAccept Interface from File Class

--- Interfaces/FileEnv/IFile.cs
+++ Interfaces/FileEnv/IFile.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Interfaces.PlayerEnv;
+
 namespace Interfaces.FileEnv
 {
-    public interface IFile
+    public interface IFile : IAccept
     {
         Guid Id { get; set; }
         string Name { get; set; }
