563b5508310fbd2fb6dbd173a27c4b8d15b4eb8f
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-25 10:34:52 +0300

    Add Playlist Type in Appropriate Assembly

---
+++ PlaylistEnv/Playlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Interfaces.PlaylistEnv;
+using Interfaces.FileEnv;
+
+namespace PlaylistEnv
+{
+    public class Playlist<TFile> : IPlaylist<TFile> where TFile : IFile 
+    {
+    }
+}
