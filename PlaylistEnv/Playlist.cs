456e630aae8ade1443004397e5d135c9e2501049
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-25 10:56:38 +0300

    Implement Main Playlist Functionality

--- PlaylistEnv/Playlist.cs
+++ PlaylistEnv/Playlist.cs
@@ -9,7 +9,7 @@ using Interfaces.FileEnv;
 
 namespace PlaylistEnv
 {
-    public class Playlist<TFile> : IPlaylist<TFile> where TFile : IFile 
+    public class Playlist<TFile> : IPlaylist<TFile> where TFile : class, IFile
     {
         private Guid _id;
         private string _name;
@@ -25,5 +25,37 @@ namespace PlaylistEnv
         public Guid Id { get => _id; set => _id = value; }
         public string Name { get => _name; set => _name = value; }
         private ICollection<TFile> MediaFiles { get => _mediaFiles; set => _mediaFiles = value; }
+
+        public void AddFile(TFile mediaFile)
+        {
+            MediaFiles.Add(mediaFile);
+        }
+
+        public void RemoveFile(Guid id)
+        {
+            MediaFiles.Remove(SearchMediaFileByID(id));
+        }
+
+        private TFile SearchMediaFileByID(Guid id)
+        {
+            foreach (var mediaFile in MediaFiles)
+            {
+                if (mediaFile.Id == id)
+                {
+                    return mediaFile;
+                }
+            }
+            return null;
+        }
+
+        public ICollection<TFile> GetAllFiles()
+        {
+            return MediaFiles;
+        }
+
+        public void Clear()
+        {
+            MediaFiles.Clear();
+        }
     }
 }
