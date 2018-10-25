03725c43d73fe18a68952935a2a44bc67e40e79a
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-25 10:41:09 +0300

    Add Main Fields and Properties to Playlist

--- PlaylistEnv/Playlist.cs
+++ PlaylistEnv/Playlist.cs
@@ -11,5 +11,12 @@ namespace PlaylistEnv
 {
     public class Playlist<TFile> : IPlaylist<TFile> where TFile : IFile 
     {
+        private Guid _id;
+        private string _name;
+        private ICollection<TFile> _mediaFiles;
+
+        public Guid Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        private ICollection<TFile> MediaFiles { get => _mediaFiles; set => _mediaFiles = value; }
     }
 }
