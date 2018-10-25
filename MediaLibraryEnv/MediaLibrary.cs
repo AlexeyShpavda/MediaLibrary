032318d644070cd68ad30182be2cf2c9735ca9fb
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-25 11:03:54 +0300

    Add MediaLibrary Type in Appropriate Assembly

---
+++ MediaLibraryEnv/MediaLibrary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Interfaces.MediaLibraryEnv;
+using Interfaces.FileEnv;
+using Interfaces.PlaylistEnv;
+using Interfaces.PlayerEnv;
+
+namespace MediaLibraryEnv
+{
+    public class MediaLibrary : IMediaLibrary
+    {
+    }
+}
