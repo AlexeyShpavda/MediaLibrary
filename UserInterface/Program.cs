6370288a430a230ab754ef2c412bd4257c9b126a
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-10-27 10:38:23 +0300

    Connect All Assemblies to User Interface

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Interfaces.FileEnv;
+using Interfaces.MediaLibraryEnv;
+using Interfaces.PlayerEnv;
+using Interfaces.PlaylistEnv;
+
+using FileEnv;
+using MediaLibraryEnv;
+using PlaylistEnv;
+using PlayerEnv;
+
 namespace UserInterface
 {
     class Program
